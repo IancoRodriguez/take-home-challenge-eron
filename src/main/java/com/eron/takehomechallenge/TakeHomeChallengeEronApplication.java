d2060fd5328a04a6720df59044adb478259c9feb
IancoRodriguez <iancorodriguez@gmail.com>
2025-11-29 16:40:56 -0300

    initial commit

---
+++ src/main/java/com/eron/takehomechallenge/TakeHomeChallengeEronApplication.java
@@ -0,0 +1,13 @@
+package com.eron.takehomechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeHomeChallengeEronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeHomeChallengeEronApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eron/takehomechallenge/TakeHomeChallengeEronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eron.takehomechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeHomeChallengeEronApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
